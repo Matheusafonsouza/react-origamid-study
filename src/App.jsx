bbe8c6f2d66ab44800a9b710bb55f3765c32981b
Matheus Afonso <matheusafonsouza@gmail.com>
2023-08-09 09:44:19 -0300

    chore: use router dom for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
 import React from 'react';
-import useForm from './hooks/useForm';
-import Input from './components/Input';
-import Product from './components/Product';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 
 const App = () => {
-  const cep = useForm('cep');
-  const email = useForm('email');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (cep.validate()) {
-      console.log('Submit');
-    } else {
-      console.log('Error');
-    };
-  };
-
   return (
-    <>
-      <Product />
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="CEP"
-          id="cep"
-          type="text"
-          placeholder="00000-000"
-          {...cep}
-        />
-        <Input
-          label="Email"
-          id="email"
-          type="email"
-          {...email}
-        />
-        <button type="submit">
-          Enviar
-        </button>
-      </form>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import useForm from '../../hooks/useForm';
+import Input from '../../components/Input';
+import Product from '../../components/Product';
+
+
+const Home = () => {
+    const cep = useForm('cep');
+    const email = useForm('email');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (cep.validate()) {
+            console.log('Submit');
+        } else {
+            console.log('Error');
+        };
+    };
+
+    return (
+        <>
+            <Product />
+            <form onSubmit={handleSubmit}>
+                <Input
+                    label="CEP"
+                    id="cep"
+                    type="text"
+                    placeholder="00000-000"
+                    {...cep}
+                />
+                <Input
+                    label="Email"
+                    id="email"
+                    type="email"
+                    {...email}
+                />
+                <button type="submit">
+                    Enviar
+                </button>
+            </form>
+        </>
+    );
+}
+
+export default Home;
