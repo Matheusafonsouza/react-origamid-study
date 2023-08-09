2f5c13335bc2efd91c3af1133636d2cdecac3916
Matheus Afonso <matheusafonsouza@gmail.com>
2023-08-09 09:48:05 -0300

    chore: header footer components and about not found pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+
 import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
